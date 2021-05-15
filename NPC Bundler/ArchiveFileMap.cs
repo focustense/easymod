74c81c5736f99664ecf365518e897f6a365bb6a9
focustense <focustense@protonmail.com>
2021-05-14 23:48:53 -0400

    Start adding BSArchive support.
    Now includes archive information in the mod map, and a utility to cache archive <--> file associations.

---
+++ NPC Bundler/ArchiveFileMap.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using XeLib.API;
+
+namespace NPC_Bundler
+{
+    // Maintains a global bidirectional map of archives (i.e. BSAs) to facegen files in those archives.
+    //
+    // Enumerating a BSA doesn't actually seem to be a very expensive operation, at least on a modern machine; but this
+    // time could still add up on lower-end machines, especially if there are hundreds of mods and some of them provide
+    // huge numbers of facegen files (Skyrim - Meshes0, obviously, but also 3DNPC and other large overhauls), and if we
+    // have to call this repeatedly for any reason.
+    static class ArchiveFileMap
+    {
+        private static Dictionary<string, IEnumerable<string>> archivesToFiles;
+        private static Dictionary<string, IEnumerable<string>> filesToArchives;
+
+        // Most code shouldn't need to call this, unless the map is going to be subsequently be used by parallel or
+        // multithreaded code that might otherwise trigger multiple concurrent initializations.
+        //
+        // It's safe to call regardless, just generally unnecessary.
+        public static void EnsureInitialized()
+        {
+            Initialize();
+        }
+
+        public static IEnumerable<string> GetArchivesContainingFile(string archivedFileName)
+        {
+            Initialize();
+            return filesToArchives.TryGetValue(archivedFileName, out IEnumerable<string> archives) ?
+                archives : Enumerable.Empty<string>();
+        }
+
+        public static IEnumerable<string> GetFilesInArchive(string archiveName)
+        {
+            Initialize();
+            return archivesToFiles.TryGetValue(archiveName, out IEnumerable<string> fileNames) ?
+                fileNames : Enumerable.Empty<string>();
+        }
+
+        private static void Initialize()
+        {
+            if (archivesToFiles != null || filesToArchives != null)
+                return;
+            var archivesWithFiles = Resources.GetLoadedContainers()
+                .Select(path => new
+                {
+                    FileName = Path.GetFileName(path),
+                    FaceGenFiles = Resources.GetContainerFiles(path, @"meshes\actors\character\facegendata\facegeom"),
+                })
+                .ToList();
+            Parallel.Invoke(
+                () =>
+                {
+                    archivesToFiles = archivesWithFiles.ToDictionary(
+                        x => x.FileName,
+                        x => x.FaceGenFiles.ToList().AsReadOnly().AsEnumerable());
+                },
+                () =>
+                {
+                    filesToArchives = archivesWithFiles
+                        .SelectMany(x => x.FaceGenFiles.Select(f => new { ArchiveFile = x.FileName, FaceGenFile = f }))
+                        .GroupBy(x => x.FaceGenFile, x => x.ArchiveFile)
+                        .ToDictionary(x => x.Key, x => x.ToList().AsReadOnly().AsEnumerable());
+                });
+        }
+    }
+}
--- NPC Bundler/ModPluginMap.cs
+++ NPC Bundler/ModPluginMap.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using XeLib.API;
 
@@ -11,6 +10,8 @@ namespace NPC_Bundler
     class ModPluginMap
     {
         private static readonly Dictionary<string, ModPluginMap> Instances = new();
+
+        enum FileType { Unknown, Plugin, Archive };
 
         public static ModPluginMap ForDirectory(string modRootDirectory)
         {
@@ -26,18 +27,52 @@ namespace NPC_Bundler
             // This actually happens, such as with "AIO" plugins created to merge individual replacers that still require
             // the loose files from the original mods.
             // So, this is a little slow, but still probably the best way.
+            var archiveFileNames = new HashSet<string>(
+                Resources.GetLoadedContainers().Select(f => Path.GetFileName(f)),
+                StringComparer.OrdinalIgnoreCase);
             var pluginFileNames = new HashSet<string>(Setup.GetLoadedFileNames(), StringComparer.OrdinalIgnoreCase);
             var modsWithPlugins = Directory.EnumerateDirectories(trimmedDirectory)
-                .Select(modDir => new {
+                .Select(modDir => new
+                {
                     Mod = Path.GetFileName(Path.TrimEndingDirectorySeparator(modDir)),
-                    Plugins = Directory.EnumerateFiles(modDir)
-                        .Where(f => pluginFileNames.Contains(Path.GetFileName(f)))
-                        .Select(f => Path.GetFileName(f))
+                    Files = Directory.EnumerateFiles(modDir)
+                        .Select(path => Path.GetFileName(path))
+                        .Select(fileName => new
+                        {
+                            FileName = fileName,
+                            FileType =
+                                pluginFileNames.Contains(fileName) ? FileType.Plugin :
+                                archiveFileNames.Contains(fileName) ? FileType.Archive :
+                                FileType.Unknown,
+                        })
+                        .Where(f => f.FileType != FileType.Unknown)
+                })
+                .Select(x => new {
+                    x.Mod,
+                    Archives = x.Files.Where(f => f.FileType == FileType.Archive).Select(f => f.FileName).ToArray(),
+                    Plugins = x.Files.Where(f => f.FileType == FileType.Plugin).Select(f => f.FileName).ToArray(),
                 })
                 .ToList();
             IDictionary<string, IEnumerable<string>> modsToPlugins = null;
             IDictionary<string, IEnumerable<string>> pluginsToMods = null;
+            IDictionary<string, IEnumerable<string>> modsToArchives= null;
+            IDictionary<string, IEnumerable<string>> archivesToMods = null;
             Parallel.Invoke(
+                () =>
+                {
+                    modsToArchives = modsWithPlugins.ToDictionary(
+                        x => x.Mod,
+                        x => x.Archives.ToList().AsReadOnly().AsEnumerable());
+                },
+                () =>
+                {
+                    archivesToMods = modsWithPlugins
+                        .SelectMany(x => x.Archives.Select(a => new { x.Mod, Archive = a }))
+                        .GroupBy(x => x.Archive, x => x.Mod)
+                        .ToDictionary(x => x.Key, x => x.ToList().AsReadOnly().AsEnumerable());
+                    foreach (var archive in archiveFileNames)
+                        archivesToMods.TryAdd(archive, Enumerable.Empty<string>());
+                },
                 () => {
                     modsToPlugins = modsWithPlugins.ToDictionary(
                         x => x.Mod,
@@ -45,26 +80,44 @@ namespace NPC_Bundler
                 },
                 () => {
                     pluginsToMods = modsWithPlugins
-                        .SelectMany(x => x.Plugins.Select(p => new { Mod = x.Mod, Plugin = p }))
+                        .SelectMany(x => x.Plugins.Select(p => new { x.Mod, Plugin = p }))
                         .GroupBy(x => x.Plugin, x => x.Mod)
                         .ToDictionary(x => x.Key, x => x.ToList().AsReadOnly().AsEnumerable());
                     foreach (var plugin in pluginFileNames)
                         pluginsToMods.TryAdd(plugin, Enumerable.Empty<string>());
                 });
-            var map = new ModPluginMap(modsToPlugins, pluginsToMods);
+            var map = new ModPluginMap(modsToPlugins, pluginsToMods, modsToArchives, archivesToMods);
             Instances.TryAdd(modRootDirectory, map);
             return map;
         }
 
+        private readonly IDictionary<string, IEnumerable<string>> archivesToMods;
+        private readonly IDictionary<string, IEnumerable<string>> modsToArchives;
         private readonly IDictionary<string, IEnumerable<string>> modsToPlugins;
         private readonly IDictionary<string, IEnumerable<string>> pluginsToMods;
 
         private ModPluginMap(
             IDictionary<string, IEnumerable<string>> modsToPlugins,
-            IDictionary<string, IEnumerable<string>> pluginsToMods)
+            IDictionary<string, IEnumerable<string>> pluginsToMods,
+            IDictionary<string, IEnumerable<string>> modsToArchives,
+            IDictionary<string, IEnumerable<string>> archivesToMods)
         {
             this.modsToPlugins = modsToPlugins;
             this.pluginsToMods = pluginsToMods;
+            this.modsToArchives = modsToArchives;
+            this.archivesToMods = archivesToMods;
+        }
+
+        public IEnumerable<string> GetArchivesForMod(string modName)
+        {
+            return modsToArchives.TryGetValue(modName, out IEnumerable<string> archives) ?
+                archives : Enumerable.Empty<string>();
+        }
+
+        public IEnumerable<string> GetModsForArchive(string archiveName)
+        {
+            return archivesToMods.TryGetValue(archiveName, out IEnumerable<string> mods) ?
+                mods : Enumerable.Empty<string>();
         }
 
         public IEnumerable<string> GetModsForPlugin(string pluginName)
