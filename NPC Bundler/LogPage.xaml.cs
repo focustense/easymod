49965cc1d39b3c919f11a37316cd9fc7e4f74712
focustense <focustense@protonmail.com>
2021-05-18 02:35:23 -0400

    Keep log entries flowing when log page is active.

--- NPC Bundler/LogPage.xaml.cs
+++ NPC Bundler/LogPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ModernWpf.Controls;
+using System.Windows;
 
 namespace NPC_Bundler
 {
@@ -7,9 +8,22 @@ namespace NPC_Bundler
     /// </summary>
     public partial class LogPage : Page
     {
+        protected LogViewModel Model => ((MainViewModel)DataContext)?.Log;
+
         public LogPage()
         {
             InitializeComponent();
+        }
+
+        private void Page_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue == null && e.OldValue is MainViewModel model)
+                model.Log.Pause();
+        }
+
+        private void Page_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Model.Resume();
         }
     }
 }
