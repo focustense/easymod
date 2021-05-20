d1208c879056a127b3d1e33aae8a3a2de4e1334d
focustense <focustense@protonmail.com>
2021-05-20 01:30:30 -0400

    Minor tweaks to Progress model/control.
    Can display error and paused states now, and has some utilities to dynamically readjust the progress interval/scale.

--- NPC Bundler/ProgressViewModel.cs
+++ NPC Bundler/ProgressViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using PropertyChanged;
+using System;
 using System.ComponentModel;
 
 namespace NPC_Bundler
@@ -8,8 +9,12 @@ namespace NPC_Bundler
         public event PropertyChangedEventHandler PropertyChanged;
 
         public int CurrentProgress { get; set; }
+        public string ErrorMessage { get; set; }
+        [DependsOn("ErrorMessage")]
+        public bool HasError => !string.IsNullOrEmpty(ErrorMessage);
         [DependsOn("CurrentProgress", "MaxProgress")]
-        public bool IsCompleted => CurrentProgress == MaxProgress;
+        public bool IsCompleted => MaxProgress > 0 && CurrentProgress == MaxProgress;
+        public bool IsPaused { get; set; }
         [DependsOn("MaxProgress")]
         public bool IsIndeterminate => MaxProgress == 0;
         public string ItemName { get; set; }
@@ -19,13 +24,32 @@ namespace NPC_Bundler
         public string StageName { get; set; }
         public string TaskName { get; init; }
 
-        public ProgressViewModel(string taskName)
+        public ProgressViewModel(string taskName, bool startPaused = false, string defaultStageName = "Not started")
         {
             TaskName = taskName;
+            StageName = defaultStageName;
+            IsPaused = startPaused;
+        }
+
+        public void AdjustRemaining(int remainingProgress, float percentOfTotal)
+        {
+            // Example: Remaining units for stage = 300, current progress = 60%, stage counts for 20% of total;
+            // New progress will be 900/1500 - i.e. 60% now, 80% after we add 300.
+            // Previous current/max are irrelevant and we can simply forget about them.
+            var previousProgressPercent = ProgressPercent;
+            MaxProgress = (int)Math.Ceiling(remainingProgress / percentOfTotal);
+            CurrentProgress = (int)(MaxProgress * previousProgressPercent);
+        }
+
+        public void JumpTo(float percent)
+        {
+            CurrentProgress = (int)Math.Round(MaxProgress * percent);
         }
 
         public void StartStage(string stageName, string itemName = "")
         {
+            // Starting a stage generally means things are happening, i.e. no longer paused
+            IsPaused = false;
             StageName = stageName;
             ItemName = itemName;
         }
