57bf4143d52ba080e44740cca4060ea739ce28ff
focustense <focustense@protonmail.com>
2021-07-24 16:22:40 -0400

    Don't track generated build date in source.
    This is part of the build and needs to be regenerated each time. Tracking it will add a lot of commit noise and isn't generally useful.

--- Focus.Apps.EasyNpc/Configuration/BuildDate.generated.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace Focus.Apps.EasyNpc.Configuration
-{
-    public static partial class AssemblyProperties
-    {
-        public static DateTime BuildTimestampUtc { get { return new DateTime(637627528017427531L, DateTimeKind.Utc); } }
-    }
-}
