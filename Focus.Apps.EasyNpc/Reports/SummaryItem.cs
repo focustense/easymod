da36ce7239e6eda7d027e77dbacba9d2ded4657c
focustense <focustense@protonmail.com>
2021-07-31 12:21:13 -0400

    Add a Summary Group component.
    The component will be used for top level info in new reports e.g. pre- and post-build reports (#37, #17, #51, #61, #78, etc.)
    
    This is especially for the pre-build report, for which the flow is becoming too unwieldy and needs to be redesigned.

---
+++ Focus.Apps.EasyNpc/Reports/SummaryItem.cs
@@ -0,0 +1,48 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Focus.Apps.EasyNpc.Reports
+{
+    public enum SummaryItemCategory
+    {
+        None,
+        CountEmpty,
+        CountExcluded,
+        CountIncluded,
+        CountFull,
+        CountUnavailable,
+        SpecialFlag,
+        StatusError,
+        StatusInfo,
+        StatusOk,
+        StatusWarning,
+    }
+
+    public class SummaryItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public SummaryItemCategory Category { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string? Value { get; set; }
+
+        public SummaryItem()
+        {
+        }
+
+        public SummaryItem(SummaryItemCategory category, string description, string? value = null)
+        {
+            Category = category;
+            Description = description;
+            Value = value;
+        }
+
+        public SummaryItem(SummaryItemCategory category, string description, Decimal value)
+            : this(category, description, value.ToString())
+        { }
+    }
+}
