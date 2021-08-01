0dc736ee4e58f9d0ac3096256e5491df996ba648
focustense <focustense@protonmail.com>
2021-08-01 13:24:22 -0400

    Add tests for the basic record analyzer.
    This analyzer is simple, but more important is that this gets a framework set up for future tests, where dummy records can easily be set up and become accessible regardless of how an analyzer might try to access them.
    
    #27

---
+++ Focus.Providers.Mutagen.Tests/Analysis/BasicRecordAnalyzerTests.cs
@@ -0,0 +1,100 @@
+﻿using Focus.Providers.Mutagen.Analysis;
+using Moq;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Skyrim;
+using Noggog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using RecordType = Focus.Analysis.Records.RecordType;
+
+namespace Focus.Providers.Mutagen.Tests.Analysis
+{
+    public class BasicRecordAnalyzerTests
+    {
+        private readonly BasicRecordAnalyzer analyzer;
+        private readonly FakeGroupCache groups;
+        private readonly RecordType recordType = RecordType.Container;
+
+        public BasicRecordAnalyzerTests()
+        {
+            groups = new FakeGroupCache();
+            analyzer = new BasicRecordAnalyzer(groups, recordType);
+        }
+
+        [Fact]
+        public void WhenRecordNotFound_ReturnsDummyInfo()
+        {
+            var analysis = analyzer.Analyze("plugin.esp", new RecordKey("plugin.esp", "000001"));
+
+            Assert.Equal("plugin.esp", analysis.BasePluginName);
+            Assert.Equal("000001", analysis.LocalFormIdHex);
+            Assert.Equal(recordType, analysis.Type);
+            Assert.False(analysis.Exists);
+            Assert.Equal(string.Empty, analysis.EditorId);
+            Assert.False(analysis.IsInjectedOrInvalid);
+            Assert.False(analysis.IsOverride);
+        }
+
+        [Fact]
+        public void WhenRecordIsWrongType_ReturnsDummyInfo()
+        {
+            groups.AddRecords<Explosion>("plugin.esp", x => { });
+            var analysis = analyzer.Analyze("plugin.esp", new RecordKey("plugin.esp", "000001"));
+
+            Assert.Equal("plugin.esp", analysis.BasePluginName);
+            Assert.Equal("000001", analysis.LocalFormIdHex);
+            Assert.Equal(recordType, analysis.Type);
+            Assert.False(analysis.Exists);
+            Assert.Equal(string.Empty, analysis.EditorId);
+            Assert.False(analysis.IsInjectedOrInvalid);
+            Assert.False(analysis.IsOverride);
+        }
+
+        [Fact]
+        public void WhenRecordFound_ReturnsRecordInfo()
+        {
+            groups.AddRecords<Container>(
+                "plugin.esp",
+                x => x.EditorID = "container1",
+                x => x.EditorID = "container2");
+            var analysis = analyzer.Analyze("plugin.esp", new RecordKey("plugin.esp", "000002"));
+
+            Assert.Equal("plugin.esp", analysis.BasePluginName);
+            Assert.Equal("000002", analysis.LocalFormIdHex);
+            Assert.Equal(recordType, analysis.Type);
+            Assert.True(analysis.Exists);
+            Assert.Equal("container2", analysis.EditorId);
+            Assert.False(analysis.IsInjectedOrInvalid);
+            Assert.False(analysis.IsOverride);
+        }
+
+        [Fact]
+        public void WhenRecordKeyIsOtherPlugin_IsOverride_IsTrue()
+        {
+            var analysis = analyzer.Analyze("plugin.esp", new RecordKey("master.esp", "000001"));
+
+            Assert.True(analysis.IsOverride);
+        }
+
+        [Fact]
+        public void WhenMasterHasRecord_IsInjectedOrInvalid_IsFalse()
+        {
+            groups.AddRecords<Container>("master.esp", x => { });
+            var analysis = analyzer.Analyze("plugin.esp", new RecordKey("master.esp", "000001"));
+
+            Assert.True(analysis.IsOverride);
+        }
+
+        [Fact]
+        public void WhenMasterMissingRecord_IsInjectedOrInvalid_IsTrue()
+        {
+            var analysis = analyzer.Analyze("plugin.esp", new RecordKey("master.esp", "000001"));
+
+            Assert.True(analysis.IsInjectedOrInvalid);
+        }
+    }
+}
---
+++ Focus.Providers.Mutagen.Tests/Analysis/FakeGroupCache.cs
@@ -0,0 +1,98 @@
+﻿using Focus.Providers.Mutagen.Analysis;
+using Loqui;
+using Moq;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Skyrim;
+using Noggog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Activator = System.Activator;
+using RecordType = Focus.Analysis.Records.RecordType;
+
+namespace Focus.Providers.Mutagen.Tests.Analysis
+{
+    class FakeGroupCache : IGroupCache
+    {
+        private static readonly Dictionary<Type, RecordType> groupTypeMap = Enum.GetValues<RecordType>()
+            .Select(t => new { RecordType = t, GroupType = t.GetGroupType() })
+            .ToDictionary(x => x.GroupType, x => x.RecordType);
+
+        private readonly Dictionary<Tuple<string, RecordType>, FakeReadOnlyCache> groups = new();
+        private readonly Dictionary<string, uint> nextIds = new();
+        private readonly Dictionary<Tuple<string, Type>, object> typedGroups = new();
+
+        public void AddRecords<T>(string pluginName, params Action<T>[] configureRecords)
+            where T : class, ISkyrimMajorRecordGetter
+        {
+            var groupGetterType = LoquiRegistration.GetRegister(typeof(T)).GetterType;
+            var recordType = groupTypeMap[groupGetterType];
+            var group = GetCache(pluginName, recordType);
+            var nextId = nextIds.TryGetValue(pluginName, out var id) ? id : 0;
+            foreach (var configure in configureRecords)
+            {
+                var formKey = new FormKey(pluginName, ++nextId);
+                var record = (T)Activator.CreateInstance(typeof(T), formKey, SkyrimRelease.SkyrimSE);
+                configure(record);
+                group.Put(record);
+            }
+            nextIds[pluginName] = nextId;
+        }
+
+        public IReadOnlyCache<ISkyrimMajorRecordGetter, FormKey> Get(string pluginName, RecordType recordType)
+        {
+            return GetCache(pluginName, recordType);
+        }
+
+        public IGroupGetter<T> Get<T>(string pluginName, Func<ISkyrimModGetter, IGroupGetter<T>> groupSelector)
+            where T : class, ISkyrimMajorRecordGetter
+        {
+            return GetGroupGetter<T>(pluginName, typeof(T)).Object;
+        }
+
+        public ISkyrimModGetter GetMod(string pluginName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MasterExists(FormKey formKey, RecordType recordType)
+        {
+            return GetCache(formKey.ModKey.FileName, recordType).ContainsKey(formKey);
+        }
+
+        private FakeReadOnlyCache GetCache(string pluginName, RecordType type)
+        {
+            var key = Tuple.Create(pluginName, type);
+            if (!groups.TryGetValue(key, out var cache))
+            {
+                cache = new FakeReadOnlyCache();
+                groups.Add(key, cache);
+            }
+            return cache;
+        }
+
+        private Mock<IGroupGetter<T>> GetGroupGetter<T>(string pluginName, Type groupType)
+            where T : class, ISkyrimMajorRecordGetter
+        {
+            var key = Tuple.Create(pluginName, groupType);
+            if (!typedGroups.TryGetValue(key, out var group))
+            {
+                var recordType = groupTypeMap[groupType];
+                var baseCache = GetCache(pluginName, recordType);
+                var baseTypedCache = baseCache.Of<T>();
+
+                var groupMock = new Mock<IGroupGetter<T>>();
+                groupMock.SetupGet(x => x.Count).Returns(() => baseCache.Count);
+                groupMock.SetupGet(x => x.FormKeys).Returns(() => baseCache.Keys);
+                groupMock.SetupGet(x => x.RecordCache).Returns(baseTypedCache);
+                groupMock.Setup(x => x.ContainsKey(It.IsAny<FormKey>()))
+                    .Returns((FormKey key) => baseCache.ContainsKey(key));
+                groupMock.Setup(x => x[It.IsAny<FormKey>()]).Returns((FormKey key) => baseTypedCache[key]);
+                groupMock.Setup(x => x.GetEnumerator()).Returns(() => baseTypedCache.Items.GetEnumerator());
+                group = groupMock;
+                typedGroups.Add(key, group);
+            }
+            return (Mock<IGroupGetter<T>>)group;
+        }
+    }
+}
