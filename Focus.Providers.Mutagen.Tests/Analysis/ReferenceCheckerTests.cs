f6dd9be6a0d9f97dd555d6635d742c7fca5eec23
focustense <focustense@protonmail.com>
2021-09-02 00:39:28 -0400

    Add reference-checking utility.
    Can be configured with expressions pointing to all or most common record expansions, and will follow all paths and report the full paths from the root record to any unresolvable references.
    
    This should help to produce useful, user-friendly (to the extent that is possible) output about plugins that have these problems and exactly why they are problems, as well as pointing directly to the records (i.e. NPCs) that need to be suppressed.
    
    Still needs to be incorporated into the other analyzers.
    
    #107

---
+++ Focus.Providers.Mutagen.Tests/Analysis/ReferenceCheckerTests.cs
@@ -0,0 +1,312 @@
+﻿using Focus.Analysis.Records;
+using Focus.Providers.Mutagen.Analysis;
+using Loqui;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Records;
+using Mutagen.Bethesda.Skyrim;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using RecordType = Focus.Analysis.Records.RecordType;
+
+namespace Focus.Providers.Mutagen.Tests.Analysis
+{
+    public class ReferenceCheckerTests
+    {
+        private readonly ReferenceChecker<INpcGetter> checker;
+        private readonly FakeGroupCache groups = new();
+
+        public ReferenceCheckerTests()
+        {
+            checker = new ReferenceChecker<INpcGetter>(groups)
+                .Follow(x => x.Class)
+                .Follow(x => x.DefaultOutfit)
+                .Follow(x => x.Keywords)
+                .Follow(x => x.HeadParts, headPart => headPart
+                    .Follow(x => x.Color)
+                    .FollowSelf(x => x.ExtraParts)
+                    .Follow(x => x.TextureSet))
+                .Follow(x => x.Race, race => race
+                    .Follow(x => x.DefaultHairColors)
+                    .Follow(x => x.Skin, skin => skin
+                        .Follow(x => x.Armature, addon => addon
+                            .Follow(x => x.AdditionalRaces)
+                            .Follow(x => x.WorldModel, g => g.AlternateTextures?.Select(t => t.NewTexture)))));
+        }
+
+        [Fact]
+        public void WhenAllReferencesValid_YieldsNoResults()
+        {
+            var classKey = AddRecord<Class>("DefaultClass");
+            var keywordKeys = AddRecords<Keyword>("Keyword1", "Keyword2");
+            var headPartKeys = groups
+                .AddRecords<HeadPart>(
+                    "main.esp",
+                    x =>
+                    {
+                        x.EditorID = "Hair";
+                        x.Color.SetTo(AddRecord<ColorRecord>("HairColor"));
+                        x.ExtraParts.AddRange(groups.AddRecords<HeadPart>("main.esp", hp =>
+                        {
+                            hp.EditorID = "Hairline";
+                            hp.Color.SetTo(AddRecord<ColorRecord>("HairlineColor"));
+                        }).ToFormKeys());
+                        x.TextureSet.SetTo(AddRecord<TextureSet>("HairTextureSet"));
+                    },
+                    x =>
+                    {
+                        x.EditorID = "Face";
+                        x.Color.SetTo(AddRecord<ColorRecord>("FaceColor"));
+                    })
+                .ToFormKeys();
+            var raceKey = groups
+                .AddRecords<Race>(
+                    "main.esp",
+                    x =>
+                    {
+                        x.EditorID = "DefaultRace";
+                        x.DefaultHairColors = new GenderedItem<IFormLinkGetter<IColorRecordGetter>>(
+                            AddRecord<ColorRecord>("DefaultMaleHairColor").AsLink<IColorRecordGetter>(),
+                            AddRecord<ColorRecord>("DefaultFemaleHairColor").AsLink<IColorRecordGetter>());
+                        x.Skin = groups
+                            .AddRecords<Armor>(
+                                "main.esp",
+                                a =>
+                                {
+                                    a.EditorID = "BodyArmor";
+                                    a.Armature.AddRange(groups.AddRecords<ArmorAddon>(
+                                        "main.esp",
+                                        d =>
+                                        {
+                                            d.EditorID = "BodyAddon";
+                                            d.AdditionalRaces.AddRange(AddRecords<Race>("AddRace1", "AddRace2"));
+                                        },
+                                        d =>
+                                        {
+                                            d.EditorID = "FeetAddon";
+                                            var maleModel = new Model();
+                                            var femaleModel = new Model
+                                            {
+                                                AlternateTextures = new(new[] {
+                                                    new AlternateTexture
+                                                    {
+                                                        NewTexture = AddRecord<TextureSet>("Feet1")
+                                                            .AsLink<ITextureSetGetter>()
+                                                    },
+                                                    new AlternateTexture
+                                                    {
+                                                        NewTexture = AddRecord<TextureSet>("Feet2")
+                                                            .AsLink<ITextureSetGetter>()
+                                                    },
+                                                }),
+                                            };
+                                            d.WorldModel = new GenderedItem<Model>(maleModel, femaleModel);
+                                        }).ToFormKeys());
+                                })
+                            .Single()
+                            .ToFormKey()
+                            .AsNullableLink<IArmorGetter>();
+                    })
+                .Single()
+                .ToFormKey();
+            var npc = new Npc(FormKey.Factory("123456:main.esp"), SkyrimRelease.SkyrimSE)
+            {
+                Class = classKey.AsLink<IClassGetter>(),
+                HeadParts = new(headPartKeys.Select(x => x.AsLink<IHeadPartGetter>())),
+                Keywords = new(keywordKeys.Select(x => x.AsLink<IKeywordGetter>())),
+                Race = raceKey.AsLink<IRaceGetter>()
+            };
+            var invalidPaths = checker.GetInvalidPaths(npc);
+
+            Assert.Empty(invalidPaths);
+        }
+
+        [Fact]
+        public void WhenReferencesInvalid_YieldsInvalidPaths()
+        {
+            var classKey = FormKey.Factory("123456:badclass.esp");
+            var keywordKeys = new[]
+            {
+                AddRecord<Keyword>("Keyword1"),
+                FormKey.Factory("123456:badkeyword.esp"),
+                AddRecord<Keyword>("Keyword2"),
+            };
+            var headPartKeys = groups
+                .AddRecords<HeadPart>(
+                    "main.esp",
+                    x =>
+                    {
+                        x.EditorID = "Hair";
+                        x.Color.SetTo(AddRecord<ColorRecord>("HairColor"));
+                        x.ExtraParts.AddRange(groups.AddRecords<HeadPart>("main.esp", hp =>
+                        {
+                            hp.EditorID = "Hairline";
+                            hp.Color.SetTo(AddRecord<ColorRecord>("HairlineColor"));
+                            hp.TextureSet.SetTo(FormKey.Factory("123456:badhairlinetextureset.esp"));
+                        }).ToFormKeys());
+                        x.ExtraParts.Add(FormKey.Factory("123456:badheadpart.esp").AsLink<IHeadPartGetter>());
+                        x.TextureSet.SetTo(AddRecord<TextureSet>("HairTextureSet"));
+                    },
+                    x =>
+                    {
+                        x.EditorID = "Face";
+                        x.Color.SetTo(FormKey.Factory("123456:badfacecolor.esp").AsLink<IColorRecordGetter>());
+                    })
+                .ToFormKeys()
+                .ToList();
+            var raceKey = groups
+                .AddRecords<Race>(
+                    "main.esp",
+                    x =>
+                    {
+                        x.EditorID = "DefaultRace";
+                        x.DefaultHairColors = new GenderedItem<IFormLinkGetter<IColorRecordGetter>>(
+                            FormKey.Factory("123456:badmalehaircolor.esp").AsLink<IColorRecordGetter>(),
+                            AddRecord<ColorRecord>("DefaultFemaleHairColor").AsLink<IColorRecordGetter>());
+                        x.Skin = groups
+                            .AddRecords<Armor>(
+                                "main.esp",
+                                a =>
+                                {
+                                    a.EditorID = "BodyArmor";
+                                    a.Armature.AddRange(groups.AddRecords<ArmorAddon>(
+                                        "main.esp",
+                                        d =>
+                                        {
+                                            d.EditorID = "BodyAddon";
+                                            d.AdditionalRaces.AddRange(AddRecords<Race>("AddRace1", "AddRace2"));
+                                            d.AdditionalRaces.Add(FormKey.Factory("123456:badadditionalrace.esp"));
+                                        },
+                                        d =>
+                                        {
+                                            d.EditorID = "FeetAddon";
+                                            var maleModel = new Model();
+                                            var femaleModel = new Model
+                                            {
+                                                AlternateTextures = new(new[] {
+                                                    new AlternateTexture
+                                                    {
+                                                        NewTexture = FormKey.Factory("123456:badfeettexture.esp")
+                                                            .AsLink<ITextureSetGetter>(),
+                                                    },
+                                                    new AlternateTexture
+                                                    {
+                                                        NewTexture = AddRecord<TextureSet>("Feet2")
+                                                            .AsLink<ITextureSetGetter>()
+                                                    },
+                                                }),
+                                            };
+                                            d.WorldModel = new GenderedItem<Model>(maleModel, femaleModel);
+                                        }).ToFormKeys());
+                                })
+                            .Single()
+                            .ToFormKey()
+                            .AsNullableLink<IArmorGetter>();
+                    })
+                .Single()
+                .ToFormKey();
+            var npc = new Npc(FormKey.Factory("123456:main.esp"), SkyrimRelease.SkyrimSE)
+            {
+                EditorID = "TestNpc",
+                Class = classKey.AsLink<IClassGetter>(),
+                HeadParts = new(headPartKeys.Select(x => x.AsLink<IHeadPartGetter>())),
+                Keywords = new(keywordKeys.Select(x => x.AsLink<IKeywordGetter>())),
+                Race = raceKey.AsLink<IRaceGetter>()
+            };
+            var invalidPaths = checker.GetInvalidPaths(npc).ToList();
+
+            Assert.Collection(
+                invalidPaths,
+                x => AssertPath(x, PathFrom(npc).Key("123456:badclass.esp", RecordType.Class)),
+                x => AssertPath(x, PathFrom(npc).Key("123456:badkeyword.esp", RecordType.Keyword)),
+                x => AssertPath(x, PathFrom(npc)
+                    .Id("Hair")
+                    .Id("Hairline")
+                    .Key("123456:badhairlinetextureset.esp", RecordType.TextureSet)),
+                x => AssertPath(x, PathFrom(npc).Id("Hair").Key("123456:badheadpart.esp", RecordType.HeadPart)),
+                x => AssertPath(x, PathFrom(npc).Id("Face").Key("123456:badfacecolor.esp", RecordType.Color)),
+                x => AssertPath(x, PathFrom(npc)
+                    .Id("DefaultRace")
+                    .Key("123456:badmalehaircolor.esp", RecordType.Color)),
+                x => AssertPath(x, PathFrom(npc)
+                    .Id("DefaultRace")
+                    .Id("BodyArmor")
+                    .Id("BodyAddon")
+                    .Key("123456:badadditionalrace.esp", RecordType.Race)),
+                x => AssertPath(x, PathFrom(npc)
+                    .Id("DefaultRace")
+                    .Id("BodyArmor")
+                    .Id("FeetAddon")
+                    .Key("123456:badfeettexture.esp", RecordType.TextureSet)));
+        }
+
+        private FormKey AddRecord<T>(string editorId)
+            where T : class, ISkyrimMajorRecord
+        {
+            return AddRecords<T>(editorId).Single();
+        }
+
+        private IEnumerable<FormKey> AddRecords<T>(params string[] editorIds)
+            where T : class, ISkyrimMajorRecord
+        {
+            foreach (var editorId in editorIds)
+                yield return groups.AddRecords<T>("main.esp", x => x.EditorID = editorId).Single().ToFormKey();
+        }
+
+        private void AssertPath(ReferencePath path, PathAssertionBuilder builder)
+        {
+            Assert.Collection(
+                path.References,
+                builder.References
+                    .Select(x => (Action<ReferenceInfo>)(r =>
+                    {
+                        Assert.Equal(r.Key, x.Key);
+                        Assert.Equal(r.EditorId, x.EditorId);
+                        Assert.Equal(r.Type, x.Type);
+                    }))
+                    .ToArray());
+        }
+
+        private PathAssertionBuilder PathFrom<T>(T record)
+            where T : ISkyrimMajorRecordGetter
+        {
+            var getterType = LoquiRegistration.GetRegister(typeof(T)).GetterType;
+            return new PathAssertionBuilder(
+                groups, new ReferenceInfo(record.FormKey.ToRecordKey(), getterType.GetRecordType(), record.EditorID));
+        }
+
+        class PathAssertionBuilder
+        {
+            public readonly List<ReferenceInfo> References = new();
+
+            private readonly FakeGroupCache groups;
+
+            public PathAssertionBuilder(FakeGroupCache groups, ReferenceInfo origin)
+            {
+                this.groups = groups;
+                References.Add(origin);
+            }
+
+            public PathAssertionBuilder Id(string editorId)
+            {
+                var key = groups.FindKeyForEditorId(editorId, out var group);
+                if (key == FormKey.Null)
+                    throw new ArgumentException($"{editorId} not found in test cache");
+                var record = group[key];
+                var getterType = LoquiRegistration.GetRegister(record.GetType()).GetterType;
+                References.Add(new ReferenceInfo(key.ToRecordKey(), getterType.GetRecordType(), editorId));
+                return this;
+            }
+
+            public PathAssertionBuilder Key(string key, RecordType type)
+            {
+                References.Add(new ReferenceInfo(RecordKey.Parse(key), type));
+                return this;
+            }
+        }
+    }
+}
---
+++ Focus.Providers.Mutagen/Analysis/ReferenceChecker.cs
@@ -0,0 +1,127 @@
+﻿using Focus.Analysis.Records;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Records;
+using Mutagen.Bethesda.Skyrim;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Focus.Providers.Mutagen.Analysis
+{
+    public interface IReferenceChecker<T>
+    {
+        IEnumerable<ReferencePath> GetInvalidPaths(T record);
+    }
+
+    public class ReferenceChecker<T> : IReferenceChecker<T>
+        where T : class, ISkyrimMajorRecordGetter
+    {
+        private readonly ConcurrentDictionary<FormKey, ReferenceInfo> cache = new();
+        private readonly IGroupCache groupCache;
+        private readonly List<Func<T, IEnumerable<IEnumerable<ReferenceInfo>>>> invalidPathSelectors = new();
+
+        public ReferenceChecker(IGroupCache groupCache)
+        {
+            this.groupCache = groupCache;
+        }
+
+        public ReferenceChecker<T> Follow<U>(
+            Func<T, IFormLinkGetter<U>?> linkSelector, Action<ReferenceChecker<U>>? configure = null)
+            where U : class, ISkyrimMajorRecordGetter
+        {
+            return Follow(r => new[] { linkSelector(r) }, configure);
+        }
+
+        public ReferenceChecker<T> Follow<U>(
+            Func<T, IGenderedItemGetter<IFormLinkGetter<U>?>?> itemSelector,
+            Action<ReferenceChecker<U>>? configure = null)
+            where U : class, ISkyrimMajorRecordGetter
+        {
+            return Follow(r =>
+            {
+                var item = itemSelector(r);
+                if (item is null)
+                    return Enumerable.Empty<IFormLinkGetter<U>>();
+                return new[] { item.Male, item.Female };
+            }, configure);
+        }
+
+        public ReferenceChecker<T> Follow<G, U>(
+            Func<T, IGenderedItemGetter<G>?> itemSelector, Func<G, IFormLinkGetter<U>?> linkSelector,
+            Action<ReferenceChecker<U>>? configure = null)
+            where U : class, ISkyrimMajorRecordGetter
+        {
+            return Follow(itemSelector, g => new[] { linkSelector(g) }, configure);
+        }
+
+        public ReferenceChecker<T> Follow<G, U>(
+            Func<T, IGenderedItemGetter<G>?> itemSelector, Func<G, IEnumerable<IFormLinkGetter<U>?>?> linksSelector,
+            Action<ReferenceChecker<U>>? configure = null)
+            where U : class, ISkyrimMajorRecordGetter
+        {
+            return Follow(r =>
+            {
+                var item = itemSelector(r);
+                if (item is null)
+                    return Enumerable.Empty<IFormLinkGetter<U>>();
+                return (linksSelector(item.Male) ?? Enumerable.Empty<IFormLinkGetter<U>?>())
+                    .Concat(linksSelector(item.Female) ?? Enumerable.Empty<IFormLinkGetter<U>?>());
+            }, configure);
+        }
+
+        public ReferenceChecker<T> Follow<U>(
+            Func<T, IEnumerable<IFormLinkGetter<U>?>?> linksSelector, Action<ReferenceChecker<U>>? configure = null)
+            where U : class, ISkyrimMajorRecordGetter
+        {
+            var subrecordChecker = new ReferenceChecker<U>(groupCache);
+            configure?.Invoke(subrecordChecker);
+            invalidPathSelectors.Add(r => CheckLinks(r, linksSelector, subrecordChecker));
+            return this;
+        }
+
+        public ReferenceChecker<T> FollowSelf(Func<T, IEnumerable<IFormLinkGetter<T>?>?> linksSelector)
+        {
+            invalidPathSelectors.Add(r => CheckLinks(r, linksSelector, this));
+            return this;
+        }
+
+        public IEnumerable<ReferencePath> GetInvalidPaths(T record)
+        {
+            return invalidPathSelectors
+                .Select(ps => ps(record))
+                .SelectMany(refLists => refLists)
+                .Select(refs => new ReferencePath(refs));
+        }
+
+        private IEnumerable<IEnumerable<ReferenceInfo>> CheckLinks<U>(
+            T record, Func<T, IEnumerable<IFormLinkGetter<U>?>?> linksSelector, ReferenceChecker<U> subrecordChecker)
+            where U : class, ISkyrimMajorRecordGetter
+        {
+            var originKey = record.FormKey.ToRecordKey();
+            var originType = typeof(T).GetRecordType();
+            var originInfo = cache.GetOrAdd(
+                record.FormKey, _ => new ReferenceInfo(originKey, originType, record.EditorID ?? string.Empty));
+            foreach (var link in linksSelector(record) ?? Enumerable.Empty<IFormLinkGetter<U>>())
+            {
+                if (link is null || link.IsNull)
+                    continue;
+                var target = link.WinnerFrom(groupCache);
+                if (target is null)
+                {
+                    var referenceInfo =
+                        cache.GetOrAdd(link.FormKey, _ =>
+                            new ReferenceInfo(link.FormKey.ToRecordKey(), link.Type.GetRecordType()));
+                    yield return new[] { originInfo, referenceInfo };
+                    continue;
+                }
+                var subrecordPaths = subrecordChecker.invalidPathSelectors
+                    .Select(ps => ps(target))
+                    .SelectMany(refLists => refLists)
+                    .Select(path => path.Prepend(originInfo));
+                foreach (var subrecordPath in subrecordPaths)
+                    yield return subrecordPath;
+            }
+        }
+    }
+}
