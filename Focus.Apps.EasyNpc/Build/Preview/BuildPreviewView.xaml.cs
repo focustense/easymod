06a4cd0d05b30e78c76a08959335b940c20a6402
focustense <focustense@protonmail.com>
2021-09-25 00:18:33 -0400

    Add smooth scrolling to build preview TOC.
    Only works in the forward direction right now - clicking on a list item will scroll to that section. Reverse still needs to be implemented, where scrolling manually changes the active list item.

--- Focus.Apps.EasyNpc/Build/Preview/BuildPreviewView.xaml.cs
+++ Focus.Apps.EasyNpc/Build/Preview/BuildPreviewView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace Focus.Apps.EasyNpc.Build.Preview
 {
@@ -23,12 +26,41 @@ namespace Focus.Apps.EasyNpc.Build.Preview
                 Model.ShowProfile(key);
         }
 
+        private void NpcsListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            ScrollToGroup(NpcsExpander);
+        }
+
         private void PluginGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton != MouseButton.Left || sender is not DataGrid dg)
                 return;
             if (dg.SelectedItem is PluginViewModel plugin)
                 Model.ShowMaster(plugin.PluginName);
+        }
+
+        private void PluginsListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            ScrollToGroup(PluginsExpander);
+        }
+
+        private void ScrollToGroup(Expander exp)
+        {
+            exp.IsExpanded = true;
+            var originPoint = exp.TranslatePoint(new Point(), ContentScrollViewer);
+            var newOffset = originPoint.Y + ContentScrollViewer.VerticalOffset;
+            if (newOffset != ContentScrollViewer.VerticalOffset)
+                Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Render, new Action(() =>
+                {
+                    ScrollAnimation.AnimateVertical(ContentScrollViewer, newOffset);
+                }));
+            
+        }
+
+        private void SummaryListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            if (ContentScrollViewer.VerticalOffset != 0)
+                ScrollAnimation.AnimateVertical(ContentScrollViewer, 0);
         }
     }
 }
---
+++ Focus.Apps.EasyNpc/ScrollAnimation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Focus.Apps.EasyNpc
+{
+    public static class ScrollAnimation
+    {
+        public static DependencyProperty TimeDurationProperty = DependencyProperty.RegisterAttached(
+            "TimeDuration", typeof(TimeSpan), typeof(ScrollAnimation), new(TimeSpan.FromMilliseconds(200)));
+
+        public static DependencyProperty VerticalOffsetProperty = DependencyProperty.RegisterAttached(
+            "VerticalOffset", typeof(double), typeof(ScrollAnimation),
+            new UIPropertyMetadata(0.0, OnVerticalOffsetChanged));
+
+        public static void AnimateVertical(ScrollViewer scrollViewer, double offset)
+        {
+            var animation = new DoubleAnimation
+            {
+                From = scrollViewer.VerticalOffset,
+                To = Math.Min(offset, scrollViewer.ScrollableHeight),
+                Duration = GetTimeDuration(scrollViewer),
+                EasingFunction = new QuarticEase(),
+            };
+            var storyboard = new Storyboard { Children = { animation } };
+            Storyboard.SetTarget(animation, scrollViewer);
+            Storyboard.SetTargetProperty(animation, new(VerticalOffsetProperty));
+            storyboard.Begin();
+        }
+
+        public static TimeSpan GetTimeDuration(FrameworkElement target)
+        {
+            return (TimeSpan)target.GetValue(TimeDurationProperty);
+        }
+
+        public static double GetVerticalOffset(FrameworkElement target)
+        {
+            return (double)target.GetValue(VerticalOffsetProperty);
+        }
+
+        public static void SetTimeDuration(FrameworkElement target, TimeSpan value)
+        {
+            target.SetValue(TimeDurationProperty, value);
+        }
+
+        public static void SetVerticalOffset(FrameworkElement target, double value)
+        {
+            target.SetValue(VerticalOffsetProperty, value);
+        }
+
+        private static void OnVerticalOffsetChanged(DependencyObject target, DependencyPropertyChangedEventArgs e)
+        {
+            if (target is not ScrollViewer scrollViewer)
+                return;
+            scrollViewer.ScrollToVerticalOffset((double)e.NewValue);
+        }
+    }
+}
