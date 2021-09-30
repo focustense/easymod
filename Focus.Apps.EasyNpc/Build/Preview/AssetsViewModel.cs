e1b5afb0e1e12e797907c5635e6b48ce94e9987f
focustense <focustense@protonmail.com>
2021-09-30 19:46:07 -0400

    Include facegen/facetint sizes in asset preview.

--- Focus.Apps.EasyNpc/Build/Preview/AssetsViewModel.cs
+++ Focus.Apps.EasyNpc/Build/Preview/AssetsViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using Focus.Analysis.Execution;
 using Focus.Analysis.Plugins;
 using Focus.Analysis.Records;
+using Focus.Apps.EasyNpc.GameData.Files;
 using Focus.Apps.EasyNpc.Profiles;
 using Focus.Apps.EasyNpc.Reports;
 using Focus.Files;
+using Focus.ModManagers;
 using PropertyChanged;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO.Abstractions;
 using System.Linq;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
@@ -15,6 +18,8 @@ using System.Reactive.Subjects;
 
 namespace Focus.Apps.EasyNpc.Build.Preview
 {
+    record FaceGenInfo(long FaceGenSizeBytes, long FaceTintSizeBytes);
+
     [AddINotifyPropertyChangedInterface]
     public class AssetsViewModel : IDisposable
     {
@@ -53,17 +58,26 @@ namespace Focus.Apps.EasyNpc.Build.Preview
                 new(SummaryItemCategory.StatusInfo, "Calculating compressed size..."),
         };
 
+        private readonly IArchiveProvider archiveProvider;
         private readonly ConcurrentDictionary<string, ulong> assetSizes = new(PathComparer.Default);
         private readonly Subject<bool> changes = new();
         private readonly Subject<bool> disposed = new();
         private readonly IFileProvider fileProvider;
+        private readonly IFileSystem fs;
         private bool isInitialSizeComputed;
+        private readonly IModRepository modRepository;
         private readonly ConcurrentDictionary<IRecordKey, IEnumerable<AssetReference>> npcAssets = new();
         private readonly Dictionary<IRecordKey, RecordAnalysisChain<NpcAnalysis>> npcChains;
+        private readonly ConcurrentDictionary<IRecordKey, FaceGenInfo> npcFaceGens = new();
 
-        public AssetsViewModel(IFileProvider fileProvider, Profile profile, LoadOrderAnalysis analysis)
+        public AssetsViewModel(
+            IFileSystem fs, IFileProvider fileProvider, IArchiveProvider archiveProvider, IModRepository modRepository,
+            Profile profile, LoadOrderAnalysis analysis)
         {
+            this.archiveProvider = archiveProvider;
             this.fileProvider = fileProvider;
+            this.fs = fs;
+            this.modRepository = modRepository;
             npcChains = analysis.ExtractChains<NpcAnalysis>(RecordType.Npc)
                 .ToDictionary(x => x.Key, RecordKeyComparer.Default);
             changes
@@ -89,6 +103,7 @@ namespace Focus.Apps.EasyNpc.Build.Preview
                     // check anyway to be safe.
                     npcAssets[chain.Key] = chain.Contains(npc.FaceOption.PluginName) ?
                         chain[npc.FaceOption.PluginName].Analysis.ReferencedAssets : Enumerable.Empty<AssetReference>();
+                    npcFaceGens[chain.Key] = GetFaceGenInfo(npc);
                     changes.OnNext(true);
                 });
         }
@@ -99,19 +114,43 @@ namespace Focus.Apps.EasyNpc.Build.Preview
             GC.SuppressFinalize(this);
         }
 
+        private FaceGenInfo GetFaceGenInfo(Npc npc)
+        {
+            var faceGenPath = FileStructure.GetFaceMeshFileName(npc);
+            var faceTintPath = FileStructure.GetFaceTintFileName(npc);
+            var candidateComponents = npc.FaceGenOverride is not null ?
+                npc.FaceGenOverride.Components :
+                modRepository.SearchForFiles(npc.FaceOption.PluginName, false).Select(x => x.ModComponent).ToList();
+            var faceGenResult = modRepository.SearchForFiles(candidateComponents, faceGenPath, true).FirstOrDefault();
+            var faceTintResult = modRepository.SearchForFiles(candidateComponents, faceTintPath, true).FirstOrDefault();
+            return new(GetFileSize(faceGenResult), GetFileSize(faceTintResult));
+        }
+
+        private long GetFileSize(ModSearchResult? result)
+        {
+            if (result is null)
+                return 0;
+            return !string.IsNullOrEmpty(result.ArchiveName) ?
+                archiveProvider.GetArchiveFileSize(
+                    fs.Path.Combine(result.ModComponent.Path, result.ArchiveName), result.RelativePath) :
+                fs.FileInfo.FromFileName(fs.Path.Combine(result.ModComponent.Path, result.RelativePath)).Length;
+        }
+
         private void RefreshAssetSizes()
         {
             var allAssets = npcAssets.Values
                 .SelectMany(refs => refs)
                 .Distinct()
                 .ToList();
-            isInitialSizeComputed = true;
-            UncompressedSizeBytes = allAssets
+            var sharedAssetsSizeBytes = allAssets
                 // AsParallel would speed this up considerably on the first run, but can make the UI seem sluggish on
                 // startup due to hogging cores, and since the user will usually take at least a few seconds to actually
                 // get to this screen, the tradeoff isn't really worth it.
                 .Select(x => assetSizes.GetOrAdd(x.NormalizedPath, path => fileProvider.GetSize(path)))
                 .Aggregate((sum, value) => sum + value);
+            var faceGenAssetsSizeBytes = npcFaceGens.Values.Sum(x => x.FaceGenSizeBytes + x.FaceTintSizeBytes);
+            isInitialSizeComputed = true;
+            UncompressedSizeBytes = sharedAssetsSizeBytes + (ulong)faceGenAssetsSizeBytes;
             MissingAssets = allAssets
                 .Where(x => !x.SourceRecordTypes.SetEquals(NonCriticalSourceRecordTypes))
                 .Where(x => assetSizes.GetOrDefault(x.NormalizedPath) == 0)
