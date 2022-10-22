587dd1ef1359d855389ab534daae232540015a62
focustense <focustense@protonmail.com>
2022-10-22 19:32:38 -0400

    Add a default dummy diffuse texture for empty texture sets.
    With this as the default, renderers without any textures loaded (e.g. unable to find or load the texture) will still display something, similar to the game and other tools.

---
+++ Focus.Graphics/DummyTextureSource.cs
@@ -0,0 +1,14 @@
+﻿namespace Focus.Graphics
+{
+    public class DummyTextureSource : ITextureSource
+    {
+        public TexturePixelFormat Format => TexturePixelFormat.BGRA;
+
+        private const int defaultColor = unchecked((int)0xcccc33ff);
+
+        public TextureData GetTextureData()
+        {
+            return new TextureData(1, 1, new[] { defaultColor });
+        }
+    }
+}
--- Focus.Graphics/TextureSet.cs
+++ Focus.Graphics/TextureSet.cs
@@ -2,12 +2,14 @@
 {
     public class TextureSet
     {
-        public ITextureSource? Diffuse { get; }
+        public static readonly TextureSet Empty = new();
+
+        public ITextureSource Diffuse { get; }
         public ITextureSource? Normal { get; }
 
         public TextureSet(ITextureSource? diffuse = null, ITextureSource? normal = null)
         {
-            Diffuse = diffuse;
+            Diffuse = diffuse ?? new DummyTextureSource();
             Normal = normal;
         }
     }
