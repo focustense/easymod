e18e761321df6c699703468f531af181db4f76fb
focustense <focustense@protonmail.com>
2022-10-22 19:42:09 -0400

    Add Bethesda extension to load NIFs as scene objects.
    Uses nifly. Supports skinning, at least under a limited set of assumptions that work on some test objects without elaborate posing. Loads textures from the BSShaderTextureSet in the NIF, based on search paths given to the loader; does not yet support BSAs, although all of the framework code for that exists elsewhere in the solution, so it should be a simple change.
    
    Only the normal Phong lighting (ambient, diffuse, specular) is supported right now. NIFs have other fancy things like cube maps, specular maps that are separate from the normal map's alpha, and a ton of important shader flags including model-space normals. These are all "future work", some are needed ASAP for facegens.

---
+++ Focus.Graphics.Bethesda/NifExtensions.cs
@@ -0,0 +1,59 @@
+﻿using nifly;
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+
+using NifVector2 = nifly.Vector2;
+using NifVector3 = nifly.Vector3;
+using Vector2 = System.Numerics.Vector2;
+using Vector3 = System.Numerics.Vector3;
+
+namespace Focus.Graphics.Bethesda
+{
+    internal static class NifExtensions
+    {
+        public static Matrix4x4 ToMat4(this MatTransform t)
+        {
+            // nifly doesn't appear to have any API for actually reading a matrix directly.
+            // Our only choice is to convert the rotation to Euler, which is going to be somewhat lossy.
+            float yaw = 0, pitch = 0, roll = 0;
+            t.rotation.ToEulerAngles(ref yaw, ref pitch, ref roll);
+            // Matrix4x4.CreateFromYawPitchRoll gives results that make no sense. This way seems inefficient
+            // but it'll work until it can be determined WTF is going on with System.Numerics.
+            // Also, System.Numerics transposes the arrangement of a transformation matrix, so we either
+            // have to transpose everything before multiplying, or multiply in reverse order and then
+            // transpose the result. The latter is unintuitive, but faster.
+            var rotation =
+                Matrix4x4.CreateRotationZ(roll)
+                * Matrix4x4.CreateRotationY(pitch)
+                * Matrix4x4.CreateRotationX(yaw);
+            var translation =
+                Matrix4x4.CreateTranslation(t.translation.x, t.translation.y, t.translation.z);
+            return rotation * translation * t.scale;
+        }
+
+        public static Vector2 ToVector2(this NifVector2 v, bool flipV = false)
+        {
+            return new Vector2(v.u, flipV ? v.v : 1 - v.v);
+        }
+
+        public static Vector3 ToVector3(this NifVector3 v)
+        {
+            return new Vector3(v.x, v.y, v.z);
+        }
+
+        public static bool TryGetBlock<T>(
+            this NiHeader header, NiRef niRef, [MaybeNullWhen(false)] out T block)
+            where T : NiObject
+        {
+            return header.TryGetBlock(niRef.index, out block);
+        }
+
+        public static bool TryGetBlock<T>(
+            this NiHeader header, uint index, [MaybeNullWhen(false)] out T block)
+            where T : NiObject
+        {
+            block = header.GetBlockById(index) as T;
+            return block is not null;
+        }
+    }
+}
---
+++ Focus.Graphics.Bethesda/NifSceneSource.cs
@@ -0,0 +1,250 @@
+﻿using Focus.Files;
+using Focus.Graphics.Formats;
+using nifly;
+using System.Collections.Immutable;
+using System.Numerics;
+
+namespace Focus.Graphics.Bethesda
+{
+    public class NifSceneSource : ISceneSource
+    {
+        private readonly Func<Task<NifFile>> openFile;
+        private readonly IEnumerable<string> textureSearchPaths;
+
+        public NifSceneSource(string fileName, IEnumerable<string> textureSearchPaths)
+            : this(() => OpenFileAsync(fileName), textureSearchPaths)
+        {
+        }
+
+        public NifSceneSource(Func<NifFile> openFile, IEnumerable<string> textureSearchPaths)
+        {
+            this.openFile = () => Task.FromResult(openFile());
+            this.textureSearchPaths = textureSearchPaths;
+        }
+
+        public NifSceneSource(Func<Task<NifFile>> openFile, IEnumerable<string> textureSearchPaths)
+        {
+            this.openFile = openFile;
+            this.textureSearchPaths = textureSearchPaths;
+        }
+
+        public async Task<IEnumerable<SceneObject>> LoadAsync()
+        {
+            using var file = await openFile();
+            return LoadAsync(file);
+        }
+
+        private IEnumerable<SceneObject> LoadAsync(NifFile file)
+        {
+            using var loader = new ObjectLoader(file, textureSearchPaths);
+            return loader.LoadObjects();
+        }
+
+        private static async Task<NifFile> OpenFileAsync(string fileName)
+        {
+            var data = await File.ReadAllBytesAsync(fileName);
+            return new NifFile(new vectoruchar(data));
+        }
+
+        // Using an inner class for this allows us to save some disposables without having to pass
+        // them around from method to method. Just a convenience.
+        class ObjectLoader : IDisposable
+        {
+            private readonly NifFile nif;
+            private readonly NiHeader header;
+            private readonly IEnumerable<string> textureSearchPaths;
+
+            public ObjectLoader(NifFile file, IEnumerable<string> textureSearchPaths)
+            {
+                nif = file;
+                header = file.GetHeader();
+                this.textureSearchPaths = textureSearchPaths;
+            }
+
+            public void Dispose()
+            {
+                header.Dispose();
+            }
+
+            public IEnumerable<SceneObject> LoadObjects()
+            {
+                var bonesById = GetBoneMap(out var defaultBoneTransforms);
+                var defaultPose = new Pose(defaultBoneTransforms.ToImmutableDictionary());
+                var meshes = new List<IMesh>();
+                using var shapes = nif.GetShapes();
+                return shapes
+                    .Select(shape => CreateMesh(shape, bonesById, defaultPose))
+                    .NotNull()
+                    .ToList();
+            }
+
+            protected SceneObject? CreateMesh(
+                NiShape shape, IReadOnlyDictionary<uint, Bone> bonesById, Pose defaultPose)
+            {
+                var meshBuilder = new SkinnedMesh.Builder();
+                if (!TrySetFacesFromVerts(shape, meshBuilder, out var outVertices))
+                    return null;
+                var boneIdsByIndex = GetBoneIdsByIndex(shape);
+                AddSkinTransformsFromShape(shape, meshBuilder, bonesById, boneIdsByIndex);
+                AddBoneWeightsFromShape(
+                    shape, meshBuilder, bonesById, boneIdsByIndex, outVertices);
+                var mesh = meshBuilder.Build();
+                mesh.ApplyPose(defaultPose);
+                var textures = GetTexturesFromShape(shape);
+                return new SceneObject(mesh, textures);
+            }
+
+            private void AddBoneWeightsFromShape(
+                NiShape shape,
+                SkinnedMesh.Builder meshBuilder,
+                IReadOnlyDictionary<uint, Bone> bonesById,
+                IReadOnlyDictionary<uint, uint> boneIdsByIndex,
+                IImmutableList<Vertex> outVertices)
+            {
+                // Bone weight lookup.
+                // nifly has a GetShapeBoneWeights function, but SWIG made a total mess of it.
+                if (!shape.IsSkinned())
+                    return;
+                using var skinInstanceRef = shape.SkinInstanceRef();
+                if (
+                    !header.TryGetBlock<BSDismemberSkinInstanceNiSkinInstance>(
+                        skinInstanceRef, out var skinInstance)
+                    || !header.TryGetBlock<NiSkinData>(skinInstance.dataRef, out var skinData))
+                {
+                    // Can't get weights.
+                    return;
+                }
+                // Weights show up in both the skin data and the partition. The partition is
+                // organized better for this purpose (actually has weights per vertex), but the skin
+                // data has higher precision. There may also be multiple partitions, so skin data is
+                // less confusing.
+                var vertexWeights =
+                    (
+                        from boneTuple in skinData.bones.Select(
+                            (boneData, boneIndex) => (boneData, boneIndex))
+                        let boneData = boneTuple.boneData
+                        let bone = bonesById[boneIdsByIndex[(uint)boneTuple.boneIndex]]
+                        from skinWeight in boneData.vertexWeights
+                        // Probably DO need to apply vertex map here. May be inconsistent with
+                        // the requirements in TrySetFacesFromVerts.
+                        let vertex = outVertices[skinWeight.index]
+                        group KeyValuePair.Create(bone, skinWeight.weight) by vertex into g
+                        select (vertex: g.Key, weights: g))
+                    .ToDictionary(x => x.vertex, x => BoneWeights.FromWeights(x.weights));
+                foreach (var vw in vertexWeights)
+                    meshBuilder.SetVertexWeights(vw.Key, vw.Value);
+            }
+
+            private void AddSkinTransformsFromShape(
+                NiShape shape, SkinnedMesh.Builder meshBuilder,
+                IReadOnlyDictionary<uint, Bone> bonesById,
+                IReadOnlyDictionary<uint, uint> boneIdsByIndex)
+            {
+                foreach (var (boneIndex, boneId) in boneIdsByIndex)
+                {
+                    var bone = bonesById[boneId];
+                    using var boneTransform = new MatTransform();
+                    if (!nif.GetShapeTransformSkinToBone(shape, boneIndex, boneTransform))
+                        continue;
+                    var invTransform = boneTransform.ToMat4();
+                    meshBuilder.SetInverseTransform(bone, invTransform);
+                }
+            }
+
+            private async Task<ITextureSource?> CreateTextureSourceAsync(string texturePath)
+            {
+                if (!Path.GetExtension(texturePath).Equals(".dds", StringComparison.OrdinalIgnoreCase))
+                    return null;
+                var normalizedPath = PathNormalizer.Default.NormalizeTexturePath(texturePath);
+                var foundPath = textureSearchPaths
+                    .Prepend("")
+                    .Select(searchPath => Path.Combine(searchPath, normalizedPath))
+                    .Where(f => File.Exists(f))
+                    .FirstOrDefault();
+                return !string.IsNullOrEmpty(foundPath)
+                    ? await DdsTextureSource.PreloadAsync(foundPath) : null;
+            }
+
+            private IReadOnlyDictionary<uint, uint> GetBoneIdsByIndex(NiShape shape)
+            {
+                using var boneIds = new vectorint();
+                nif.GetShapeBoneIDList(shape, boneIds);
+                return boneIds
+                    .Select((id, index) => (index, id))
+                    .ToDictionary(x => (uint)x.index, x => (uint)x.id);
+            }
+
+            private IReadOnlyDictionary<uint, Bone> GetBoneMap(
+                out IDictionary<Bone, Matrix4x4> defaultTransforms)
+            {
+                using var childRefs = new setNiRef();
+                nif.GetRootNode().GetChildRefs(childRefs);
+                var bones = new Dictionary<uint, Bone>();
+                defaultTransforms = new Dictionary<Bone, Matrix4x4>();
+                foreach (var childRef in childRefs)
+                {
+                    if (!header.TryGetBlock<NiNode>(childRef, out var boneNode))
+                        continue;
+                    var boneName = boneNode.name.get();
+                    var bone = new Bone(boneName);
+                    bones.Add(childRef.index, bone);
+                    var boneGlobalTransform = new MatTransform();
+                    nif.GetNodeTransformToGlobal(bone.Name, boneGlobalTransform);
+                    defaultTransforms.Add(bone, boneGlobalTransform.ToMat4());
+                }
+                return bones;
+            }
+
+            private async Task<TextureSet> GetTexturesFromShape(NiShape shape)
+            {
+                if (!shape.HasShaderProperty() ||
+                    !header.TryGetBlock<NiShader>(shape.ShaderPropertyRef(), out var shader) ||
+                    !shader.HasTextureSet() ||
+                    !header.TryGetBlock<BSShaderTextureSet>(shader.TextureSetRef(), out var textureSet))
+                    return TextureSet.Empty;
+                var textureList = textureSet.textures.items().Select(x => x.get()).ToList();
+                // We'll add more texture types later; currently only support diffuse and normal.
+                var diffuseTask = CreateTextureSourceAsync(textureList[0]);
+                var normalTask = CreateTextureSourceAsync(textureList[1]);
+                return new TextureSet(await diffuseTask, await normalTask);
+            }
+
+            private bool TrySetFacesFromVerts(
+                NiShape shape, SkinnedMesh.Builder meshBuilder,
+                out IImmutableList<Vertex> outVertices)
+            {
+                using var triangles = new vectorTriangle();
+                // Unsure if we need to do the equivalent of ApplyMapToTriangles here (actual
+                // function is not available from SWIG) or if it's already incorporated into the
+                // line below.
+                shape.GetTriangles(triangles);
+                using var verts = new vectorVector3();
+                using var uvs = new vectorVector2();
+                if (!nif.GetVertsForShape(shape, verts) || !nif.GetUvsForShape(shape, uvs))
+                {
+                    outVertices = ImmutableList<Vertex>.Empty;
+                    return false;
+                }
+                using var normals = nif.GetNormalsForShape(shape);
+                var faceVertices = Enumerable.Range(0, verts.Count)
+                        .Select(i => MakeVertex(
+                            verts[i], normals?[i] ?? new nifly.Vector3(), uvs[i]))
+                        .ToImmutableList();
+                meshBuilder.SetFaces(
+                    from i in Enumerable.Range(0, triangles.Count)
+                    let tri = triangles[i]
+                    let v1 = faceVertices[tri.p1]
+                    let v2 = faceVertices[tri.p2]
+                    let v3 = faceVertices[tri.p3]
+                    select new Face(new[] { v1, v2, v3 }));
+                outVertices = faceVertices;
+                return true;
+            }
+
+            private static Vertex MakeVertex(nifly.Vector3 point, nifly.Vector3 normal, nifly.Vector2 uv)
+            {
+                return new Vertex(point.ToVector3(), normal.ToVector3(), uv.ToVector2());
+            }
+        }
+    }
+}
