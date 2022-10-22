686d85a1c23e697887a4cad20926f814dec8aec5
focustense <focustense@protonmail.com>
2022-10-22 14:53:56 -0400

    Fix unsafe tangent/bitangent computations.
    The popular algorithm doesn't take into account multiple vertices with the same UV coordinate. Under these circumstances, the r-value goes to infinity and tangents/bitangents also become infinity (or NaN, after orthogonalization).
    
    A workaround for the same problem is presented at https://github.com/assimp/assimp/issues/230, although the second part of the logic isn't very clear - there doesn't appear to be any situation that could lead to a valid tangent but invalid bitangent, or vice versa - so that part of the logic is not implemented, only resetting the UV deltas when `r` is invalid.
    
    Also completes the orthogonalization so that bitangents are also made orthogonal to both the normals and tangents. Doesn't seem to make a huge difference, but should technically give smoother lighting.

--- Focus.Graphics/Tri.cs
+++ Focus.Graphics/Tri.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Diagnostics;
+using System.Numerics;
 
 namespace Focus.Graphics
 {
@@ -11,12 +12,25 @@ namespace Focus.Graphics
             var deltaUV1 = b.UV - a.UV;
             var deltaUV2 = c.UV - a.UV;
             var r = 1.0f / (deltaUV1.X * deltaUV2.Y - deltaUV1.Y * deltaUV2.X);
+            if (float.IsInfinity(r))
+            {
+                // We need 3 distinct UV coordinates. If any 2 are the same, then either one of the
+                // deltaUVs above will be 0, or both deltaUVs will be the same vector.
+                // In either case, d1.x * d2.y will equal d1.y * d2.x and 1/d gets us infinity.
+                // Other libraries (e.g. assimp) seem to work around this by assuming default UV
+                // direction in this case.
+                deltaUV1 = new Vector2(1, 0);
+                deltaUV2 = new Vector2(0, 1);
+                r = 1; // 1 * 1 - 0 * 0
+            }
             var tangent = (deltaPos1 * deltaUV2.Y - deltaPos2 * deltaUV1.Y) * r;
             var bitangent = (deltaPos2 * deltaUV1.X - deltaPos1 * deltaUV2.X) * r;
+            if (float.IsInfinity(tangent.X))
+                Debugger.Break();
             return new Tri(
-                new VertexData(a.Point, a.Normal, Ortho(tangent, a.Normal), bitangent, a.UV),
-                new VertexData(b.Point, b.Normal, Ortho(tangent, b.Normal), bitangent, b.UV),
-                new VertexData(c.Point, c.Normal, Ortho(tangent, c.Normal), bitangent, c.UV));
+                Orthogonalize(a.Point, a.Normal, tangent, bitangent, a.UV),
+                Orthogonalize(b.Point, b.Normal, tangent, bitangent, b.UV),
+                Orthogonalize(c.Point, c.Normal, tangent, bitangent, c.UV));
         }
 
         private readonly VertexData a;
@@ -37,9 +51,20 @@ namespace Focus.Graphics
             yield return c;
         }
 
-        private static Vector3 Ortho(Vector3 tangent, Vector3 normal)
+        private static VertexData Orthogonalize(
+            Vector3 point, Vector3 normal, Vector3 tangent, Vector3 bitangent, Vector2 uv)
         {
-            return Vector3.Normalize(tangent - normal * Vector3.Dot(normal, tangent));
+            Vector3 origTangent = tangent;
+            // Gram-Schmidt: First make tangent orthogonal to normal
+            tangent = Vector3.Normalize(tangent - normal * Vector3.Dot(normal, tangent));
+            // Make bitangent orthogonal to normal and to tangent
+            bitangent = Vector3.Normalize(bitangent - normal * Vector3.Dot(normal, bitangent));
+            bitangent = Vector3.Normalize(bitangent - tangent * Vector3.Dot(tangent, bitangent));
+            if (Vector3.Dot(Vector3.Cross(normal, tangent), bitangent) < 0)
+                tangent *= -1.0f;
+            if (float.IsNaN(tangent.X))
+                Debugger.Break();
+            return new VertexData(point, normal, tangent, bitangent, uv);
         }
     }
 }
