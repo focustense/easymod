d8b7c7d747241aae76e10a952b4643b20e6b68a6
focustense <focustense@protonmail.com>
2022-10-23 11:14:13 -0400

    Add a customizable file provider.
    `GameFileProvider` is the canonical version but sometimes, especially in standalone or test apps, it helps to load specific paths and/or BSAs. This is also how tools like NifSkope and Outfit Studio behave, they look at the environment to see what's available but allow the user to pick specific BSAs in order to improve performance. We don't necessarily want to search every BSA.

---
+++ Focus.Files/FileProviderBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO.Abstractions;
+
+namespace Focus.Files
+{
+    public class FileProviderBuilder
+    {
+        private readonly IFileSystem fs;
+        private readonly IArchiveProvider archiveProvider;
+        private readonly List<IFileProvider> fileProviders = new();
+
+        public FileProviderBuilder(IArchiveProvider archiveProvider)
+            : this(new FileSystem(), archiveProvider)
+        {
+        }
+
+        public FileProviderBuilder(IFileSystem fs, IArchiveProvider archiveProvider)
+        {
+            this.fs = fs;
+            this.archiveProvider = archiveProvider;
+        }
+
+        public FileProviderBuilder AddArchive(string archivePath)
+        {
+            fileProviders.Add(new ArchiveFileProvider(archiveProvider, archivePath));
+            return this;
+        }
+
+        public FileProviderBuilder AddDirectory(string directoryPath)
+        {
+            fileProviders.Add(new DirectoryFileProvider(fs, directoryPath));
+            return this;
+        }
+
+        public IAsyncFileProvider Build()
+        {
+            return new CascadingFileProvider(fileProviders);
+        }
+    }
+}
